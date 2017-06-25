d0121f923a80e0020a888504f2e59d85cb3eeac9
aichbauer <rudolfson.junior@gmail.com>
2017-06-26 01:00:30 +0200

    Feat: validate function, return token and user

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -3,18 +3,73 @@ const authService = require('../services/auth.service');
 const bcryptService = require('../services/bcrypt.service');
 
 const UserController = () => {
-  const create = (req, res) => {
+  const register = (req, res) => {
+    console.log(req.body);
     const body = req.body;
 
-    User
-      .create({
-        username: body.username,
-        password: body.password,
-      })
-      .then((user) => res.status(200).json({ user }))
-      .catch((err) => {
-        console.log(err);
-        return res.status(500).json({ msg: 'Internal server error' });
+    console.log(body);
+
+    if (body.password === body.password2) {
+      return User
+        .create({
+          email: body.email,
+          password: body.password,
+        })
+        .then((user) => {
+          const token = authService.issue({ id: user.id });
+
+          return res.status(200).json({ token, user });
+        })
+        .catch((err) => {
+          console.log(err);
+          return res.status(500).json({ msg: 'Internal server error' });
+        });
+    }
+
+    return res.status(400).json({ msg: 'Passwords don\'t match' });
+  };
+
+  const login = (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (email && password) {
+      User
+        .findOne({
+          where: {
+            email,
+          },
+        })
+        .then((user) => {
+          if (!user) {
+            return res.status(400).json({ msg: 'Bad Request: User not found' });
+          }
+
+          if (bcryptService.comparePassword(password, user.password)) {
+            const token = authService.issue({ id: user.id });
+
+            return res.status(200).json({ token, user });
+          }
+
+          return res.status(401).json({ msg: 'Unauthorized' });
+        })
+        .catch((err) => {
+          console.log(err);
+          return res.status(500).json({ msg: 'Internal server error' });
+        });
+    }
+  };
+
+  const validate = (req, res) => {
+    const tokenToVerify = req.body.token;
+
+    authService
+      .verify(tokenToVerify, (err) => {
+        if (err) {
+          return res.status(401).json({ isvalid: false, err: 'Invalid Token!' });
+        }
+
+        return res.status(200).json({ isvalid: true });
       });
   };
 
@@ -28,39 +83,12 @@ const UserController = () => {
       });
   };
 
-  const login = (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    if (username && password) {
-      User
-        .findOne({
-          where: {
-            username,
-          },
-        })
-        .then((user) => {
-          if (!user) {
-            return res.status(400).json({ msg: 'Bad Request: User not found' });
-          }
-
-          if (bcryptService.comparePassword(password, user.password)) {
-            return res.status(200).json({ token: authService.issue({ id: user.id }) });
-          }
-
-          return res.status(401).json({ msg: 'Unauthorized' });
-        })
-        .catch((err) => {
-          console.log(err);
-          return res.status(500).json({ msg: 'Internal server error' });
-        });
-    }
-  };
 
   return {
-    getAll,
-    create,
+    register,
     login,
+    validate,
+    getAll,
   };
 };
 
